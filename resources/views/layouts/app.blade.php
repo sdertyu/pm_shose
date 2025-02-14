9af9fa4d6a0117478d9cd6db594ae610f6cf2bf5
PNgo <114852769+sdertyu@users.noreply.github.com>
2025-02-14 15:33:41 +0700

    init zay theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PM Shoes</title>
 
+    <link rel="apple-touch-icon" href="{{ asset('img/theme/apple-icon.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/theme/templatemo.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/theme/custom.css') }}">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap"> --}}
+    <link rel="stylesheet" href="{{ asset('css/fontawesome/css/all.min.css') }}">
+    
 </head>
 
 <body>
@@ -21,6 +32,12 @@
     <div class="footer">
         @include('layouts.footer')
     </div>
+
+    <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+    {{-- <script src="{{ asset('js/theme/jquery-migrate-1.2.1.min.js"') }}></script> --}}
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/theme/templatemo.js') }}"></script>
+    <script src="{{ asset('js/theme/custom.js') }}"></script>
 
 </body>
 
